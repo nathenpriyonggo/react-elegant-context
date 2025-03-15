6d7f1d7e6c139574e9953c7640889f255d3b6be0
nathen-win <rpriyo.uic.edu>
2025-03-15 18:37:38 -0500

    Added context to remaining files

--- src/App.jsx
+++ src/App.jsx
@@ -72,6 +72,7 @@ function App() {
   const ctxValue = {
     items: shoppingCart.items,
     addItemToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity,
   };
   
   return (
@@ -83,7 +84,7 @@ function App() {
       <Shop>
         {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
-            <Product {...product} onAddToCart={handleAddItemToCart} />
+            <Product {...product}/>
           </li>
         ))}
       </Shop>
