7d915ddf4f740c6d4da4ebbaeffd78ae4f6ae3cd
Nathen Priyonggo <nagapri16@gmail.com>
2025-03-17 00:36:27 -0500

    Implemented useReducer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { useContext, useRef } from 'react';
 import CartModal from './CartModal.jsx';
 import { CartContext } from '../store/shopping-cart-context.jsx';
 
-export default function Header({ cart, onUpdateCartItemQuantity }) {
+export default function Header() {
   const modal = useRef();
   const { items } = useContext(CartContext);
 
